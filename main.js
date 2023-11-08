15ae921be0bdc8d5fb2645ec0206ebfcf7bc2baa
BHUVAN <90752537+b10huvan@users.noreply.github.com>
2023-11-08 18:25:08 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var song = "";
+
+function setup(){
+canvas = createCanvas(600,500);
+canvas.center();
+video = createCapture(VIDEO);
+video.hide();
+}
+function preload(){
+song = loadSound("music.mp3");
+}
+function draw(){
+image(video,0,0,600,500);
+}
+function play25(){
+    song.play();
+    
+}
